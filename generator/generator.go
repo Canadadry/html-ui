c07770b89bbdb71d6c4dfd88dab71e085b444b69
Jérôme MOUREY <jerome@everycheck.fr>
2022-03-09 12:58:30 +0100

    allow error in generator

--- generator/generator.go
+++ generator/generator.go
@@ -11,7 +11,10 @@ func Generate(w io.Writer, el ast.El) error {
 	gen := generator{
 		css: map[string]struct{}{},
 	}
-	body := gen.generate([]ast.El{el})
+	body, err := gen.generate([]ast.El{el})
+	if err != nil {
+		return err
+	}
 	head := generateHead(gen.css)
 	d := html.Document{
 		Head: head,
@@ -32,167 +35,94 @@ type generator struct {
 	mode mode
 }
 
-func (g *generator) generate(el []ast.El) []html.Tag {
+func (g *generator) generate(el []ast.El) ([]html.Tag, error) {
 	out := make([]html.Tag, 0, len(el))
 	for _, item := range el {
+		var err error
+		var child html.Tag
 		switch item.Type {
 		case ast.TypeElLayout:
-			out = append(out, g.generateLayout(item))
+			child, err = g.generateLayout(item)
 		case ast.TypeElColumn:
-			out = append(out, g.generateColumn(item))
+			child, err = g.generateColumn(item)
 		case ast.TypeElRow:
-			out = append(out, g.generateRow(item))
+			child, err = g.generateRow(item)
 		case ast.TypeElEl:
-			out = append(out, g.generateEl(item))
+			child, err = g.generateEl(item)
 		case ast.TypeElImage:
-			out = append(out, g.generateImage(item))
+			child, err = g.generateImage(item)
 		case ast.TypeElText:
-			out = append(out, g.generateText(item.Content))
+			child, err = g.generateText(item.Content)
+		default:
+			return nil, fmt.Errorf("cannot generate '%s' : unknown type", item.Type)
 		}
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, child)
 	}
-	return out
+	return out, nil
 }
 
-func (g *generator) generateLayout(el ast.El) html.Tag {
+func (g *generator) generateLayout(el ast.El) (html.Tag, error) {
+	children, err := g.generate(el.Children)
 	return html.Div(
 		html.Attributes{html.AttributeClass: "bg-255-255-255-0 fc-0-0-0-255 font-size-20 font-open-sanshelveticaverdanasans-serif s e ui s e"},
-		g.generate(el.Children)...,
-	)
+		children...,
+	), err
 }
 
-func (g *generator) generateColumn(el ast.El) html.Tag {
+func (g *generator) generateColumn(el ast.El) (html.Tag, error) {
 	classes := g.parseAttribute(el.Attr, UniqueClassesFrom("s c ct cl"))
 	g.mode = modeColumn
+	children, err := g.generate(el.Children)
 	return html.Div(
 		html.Attributes{html.AttributeClass: classes},
-		g.generate(el.Children)...,
-	)
+		children...,
+	), err
 }
 
-func (g *generator) generateRow(el ast.El) html.Tag {
+func (g *generator) generateRow(el ast.El) (html.Tag, error) {
 	classes := g.parseAttribute(el.Attr, UniqueClassesFrom("s r cl ccy"))
 	g.mode = modeColumn
+	children, err := g.generate(el.Children)
 	return html.Div(
 		html.Attributes{html.AttributeClass: classes},
-		g.generate(el.Children)...,
-	)
+		children...,
+	), err
 }
 
-func (g *generator) parseAttribute(attrs []ast.Attribute, base UniqueClasses) string {
-	hasWidthAttr := false
-	hasHeightAttr := false
-	for _, attr := range attrs {
-		class := ""
-		switch attr.Type {
-		case ast.TypeAttrSpacing:
-			class = fmt.Sprintf("spacing-%s-%s", attr.Value, attr.Value)
-		case ast.TypeAttrFontSize:
-			class = fmt.Sprintf("font-size-%s", attr.Value)
-		case ast.TypeAttrBorderWidth:
-			class = fmt.Sprintf("b-%s", attr.Value)
-		case ast.TypeAttrBorderRounded:
-			class = fmt.Sprintf("br-%s", attr.Value)
-		case ast.TypeAttrPadding:
-			class = fmt.Sprintf("p-%s", attr.Value)
-		case ast.TypeAttrBgColor:
-			class = fmt.Sprintf("bg-%d-%d-%d-255", attr.Color.R, attr.Color.G, attr.Color.B)
-		case ast.TypeAttrFontColor:
-			class = fmt.Sprintf("fc-%d-%d-%d-255", attr.Color.R, attr.Color.G, attr.Color.B)
-		case ast.TypeAttrBorderColor:
-			class = fmt.Sprintf("bc-%d-%d-%d-255", attr.Color.R, attr.Color.G, attr.Color.B)
-		case ast.TypeAttrWidth:
-			hasWidthAttr = true
-			switch attr.Size.Type() {
-			case ast.SizePxType:
-				class = fmt.Sprintf("width-px-%d", attr.Size.Get())
-				base.Add("we")
-			case ast.SizePortionType:
-				class = fmt.Sprintf("width-fill-%d", attr.Size.Get())
-				base.Add("wfp")
-			case ast.SizeFillType:
-				class = "wf"
-			}
-		case ast.TypeAttrHeight:
-			hasHeightAttr = true
-			switch attr.Size.Type() {
-			case ast.SizePxType:
-				class = fmt.Sprintf("height-px-%d", attr.Size.Get())
-				base.Add("he")
-			case ast.SizePortionType:
-				class = fmt.Sprintf("height-fill-%d", attr.Size.Get())
-				base.Add("hfp")
-			case ast.SizeFillType:
-				class = "hf"
-			}
-		case ast.TypeAttrAlign:
-			switch attr.AlignX {
-			case "left":
-				base.Add("av")
-				base.Add("al")
-			case "right":
-				base.Add("av")
-				base.Add("ar")
-			case "centerX":
-				base.Add("av")
-				base.Add("cx")
-			}
-			switch attr.AlignY {
-			case "top":
-				base.Add("ah")
-				base.Add("at")
-			case "bottom":
-				base.Add("ah")
-				base.Add("ab")
-			case "centerY":
-				base.Add("ah")
-				base.Add("cy")
-			}
-		default:
-			continue
-		}
-		if class != "" {
-			base.Add(class)
-			g.css.Add(class)
-		}
-	}
-	if !hasWidthAttr {
-		base.Add("wc")
-	}
-	if !hasHeightAttr {
-		base.Add("hc")
-	}
-	return base.String()
-}
-
-func (g *generator) generateEl(el ast.El) html.Tag {
+func (g *generator) generateEl(el ast.El) (html.Tag, error) {
 	if len(el.Children) > 0 && el.Children[0].Type == ast.TypeElText {
 		return g.generateElText(el)
 	}
 	classes := g.parseAttribute(el.Attr, UniqueClassesFrom("s e"))
 	g.mode = modeNormal
+	children, err := g.generate(el.Children)
 	tag := html.Div(
 		html.Attributes{html.AttributeClass: classes},
-		g.generate(el.Children)...,
+		children...,
 	)
 	if len(el.Children) == 0 {
-		return html.Inline(tag)
+		return html.Inline(tag), err
 	}
-	return tag
+	return tag, err
 }
 
-func (g *generator) generateElText(el ast.El) html.Tag {
+func (g *generator) generateElText(el ast.El) (html.Tag, error) {
 	if len(el.Attr) == 0 {
 		return g.generateText(el.Children[0].Content)
 	}
 	classes := g.parseAttribute(el.Attr, UniqueClassesFrom("s e"))
 	g.mode = modeNormal
+	children, err := g.generateText(el.Children[0].Content)
 	return html.Div(
 		html.Attributes{html.AttributeClass: classes},
-		g.generateText(el.Children[0].Content),
-	)
+		children,
+	), err
 }
 
-func (g *generator) generateImage(el ast.El) html.Tag {
+func (g *generator) generateImage(el ast.El) (html.Tag, error) {
 	src := ""
 	alt := ""
 	for _, attr := range el.Attr {
@@ -222,15 +152,15 @@ func (g *generator) generateImage(el ast.El) html.Tag {
 				html.AttributeAlt:   alt,
 			},
 		),
-	)
+	), nil
 }
 
-func (g *generator) generateText(txt string) html.Tag {
+func (g *generator) generateText(txt string) (html.Tag, error) {
 	class := "s t wf hf"
 	if g.mode == modeColumn {
 		class = "s t wc hc"
 	}
 	return html.Inline(html.Div(html.Attributes{html.AttributeClass: class},
 		html.Text(txt),
-	))
+	)), nil
 }
---
+++ generator/generator_parse_attr.go
@@ -0,0 +1,92 @@
+package generator
+
+import (
+	"app/ast"
+	"fmt"
+)
+
+func (g *generator) parseAttribute(attrs []ast.Attribute, base UniqueClasses) string {
+	hasWidthAttr := false
+	hasHeightAttr := false
+	for _, attr := range attrs {
+		class := ""
+		switch attr.Type {
+		case ast.TypeAttrSpacing:
+			class = fmt.Sprintf("spacing-%s-%s", attr.Value, attr.Value)
+		case ast.TypeAttrFontSize:
+			class = fmt.Sprintf("font-size-%s", attr.Value)
+		case ast.TypeAttrBorderWidth:
+			class = fmt.Sprintf("b-%s", attr.Value)
+		case ast.TypeAttrBorderRounded:
+			class = fmt.Sprintf("br-%s", attr.Value)
+		case ast.TypeAttrPadding:
+			class = fmt.Sprintf("p-%s", attr.Value)
+		case ast.TypeAttrBgColor:
+			class = fmt.Sprintf("bg-%d-%d-%d-255", attr.Color.R, attr.Color.G, attr.Color.B)
+		case ast.TypeAttrFontColor:
+			class = fmt.Sprintf("fc-%d-%d-%d-255", attr.Color.R, attr.Color.G, attr.Color.B)
+		case ast.TypeAttrBorderColor:
+			class = fmt.Sprintf("bc-%d-%d-%d-255", attr.Color.R, attr.Color.G, attr.Color.B)
+		case ast.TypeAttrWidth:
+			hasWidthAttr = true
+			switch attr.Size.Type() {
+			case ast.SizePxType:
+				class = fmt.Sprintf("width-px-%d", attr.Size.Get())
+				base.Add("we")
+			case ast.SizePortionType:
+				class = fmt.Sprintf("width-fill-%d", attr.Size.Get())
+				base.Add("wfp")
+			case ast.SizeFillType:
+				class = "wf"
+			}
+		case ast.TypeAttrHeight:
+			hasHeightAttr = true
+			switch attr.Size.Type() {
+			case ast.SizePxType:
+				class = fmt.Sprintf("height-px-%d", attr.Size.Get())
+				base.Add("he")
+			case ast.SizePortionType:
+				class = fmt.Sprintf("height-fill-%d", attr.Size.Get())
+				base.Add("hfp")
+			case ast.SizeFillType:
+				class = "hf"
+			}
+		case ast.TypeAttrAlign:
+			switch attr.AlignX {
+			case "left":
+				base.Add("av")
+				base.Add("al")
+			case "right":
+				base.Add("av")
+				base.Add("ar")
+			case "centerX":
+				base.Add("av")
+				base.Add("cx")
+			}
+			switch attr.AlignY {
+			case "top":
+				base.Add("ah")
+				base.Add("at")
+			case "bottom":
+				base.Add("ah")
+				base.Add("ab")
+			case "centerY":
+				base.Add("ah")
+				base.Add("cy")
+			}
+		default:
+			continue
+		}
+		if class != "" {
+			base.Add(class)
+			g.css.Add(class)
+		}
+	}
+	if !hasWidthAttr {
+		base.Add("wc")
+	}
+	if !hasHeightAttr {
+		base.Add("hc")
+	}
+	return base.String()
+}
