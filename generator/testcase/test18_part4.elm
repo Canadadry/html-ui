ebcd01305d7e6ca11111471aa0d216b4e0ec4833
Jérôme MOUREY <jerome@everycheck.fr>
2022-03-11 15:34:40 +0100

    begining of file (first input match test)

---
+++ generator/testcase/test18_part4.elm
@@ -0,0 +1,48 @@
+<layout>
+	<column spacing="30">
+		<el bg-color="rgb(240,0,245)" font-color="white" border-rounded="3" padding="30">
+			hello
+		</el>
+		<el bg-color="rgb(240,0,245)" font-color="white" border-rounded="3" padding="30">
+			hello
+		</el>
+		<form action="/someurl" method="POST" name="test-form">
+			<el bg-color="rgb(240,0,245)" font-color="white" border-rounded="3" padding="30">
+				hello
+			</el>
+			<input type="text" name="input" value="value">
+				<label position="right">
+					<el bg-color="rgb(240,0,245)" font-color="white" border-rounded="3" padding="30">
+						label right
+					</el>
+				</label>
+			</input>
+			<input type="text" name="input" value="value">
+				<label position="left">
+					<el bg-color="rgb(240,0,245)" font-color="white" border-rounded="3" padding="30">
+						label left
+					</el>
+				</label>
+			</input>
+			<input type="text" name="input" value="value">
+				<label position="above">
+					<el bg-color="rgb(240,0,245)" font-color="white" border-rounded="3" padding="30">
+						label above
+					</el>
+				</label>
+			</input>
+			<input type="text" name="input" value="value">
+				<label position="below">
+					<el bg-color="rgb(240,0,245)" font-color="white" border-rounded="3" padding="30">
+						label below
+					</el>
+				</label>
+			</input>
+			<button bg-color="rgb(238,0,0)" focus-bg-color="rgb(0,255,0)" padding="30" name="test" value="button">
+				<el bg-color="rgb(240,0,245)" font-color="white" border-rounded="3" padding="30">
+					My Button
+				</el>
+			</button>
+		</form>
+	</column>
+</layout>
